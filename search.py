fa18ffa79ea56e0f4852975507a7b030421b0c08
Lijia Li <lil@oxy.edu>
2018-02-03 20:51:04 -0800

    add test on python built-in xml lib

--- search.py
+++ search.py
@@ -1,4 +1,17 @@
+import xml.etree.ElementTree as ET
 
-encodedFile=open("Chopin.xml", 'r') #Opens MEI file in read mode
+# parse through the given file & pass value to tree and root
+tree = ET.parse('Chopin.xml')
+root = tree.getroot()
 
-print(encodedFile.read())
+
+### below are tests, feel free to delete
+    # these tests only able to spit memory location as result so far
+print(root.keys)
+
+for child in root:
+    print(child.tag, child.attrib, child.text)
+
+element_ls= ET.SubElement(root, "persName")
+print(element_ls)
+
