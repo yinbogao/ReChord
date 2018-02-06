b40c2b499a7781c1f5df26c13bce53a49ab5b1d3
Unknown <nickbethune@hotmail.com>
2018-02-05 23:23:45 -0800

    added note search function
    function takes in XPath etree element and a list of notes to search and returns the index where the searched sequence can be found

--- search.py
+++ search.py
@@ -37,13 +37,28 @@ def getNotesList(root, i, list):
     #check if element is a note, then if it is, add it's letter to the list.
     if(root.tag.replace("{http://www.music-encoding.org/ns/mei}", "")=='note'):
         list.append(root.attrib['pname'])
-        print(root.attrib['pname'])
     if len(root):
         for child in root:
             getNotesList(child, i+1, list)
     return list
 
+def noteSearch(inputList, root):
+    #inputList = search criteria, root = MEI file wh/ is being searched
+    #returns index where sequence was found, -1 if not found
 
-for i in getNotesList(root, 0, []):
-    print(i, end=", ")
+    #noteList = ['a', 'b', 'c', 'd', 'e', 'f']
+    noteList = getNotesList(root, 0, [])
+    for i in range(0, len(noteList)-len(inputList)+1):
+        j=0
+        while(noteList[i+j]==inputList[j]):
+            if(j == len(inputList) - 1):
+                return i
+            j+=1
+    return -1
+criteria=['e', 'f']
+print(getNotesList(root, 0, [])[noteSearch(['e','f'], root)])
+
+
+#for i in getNotesList(root, 0, []):
+ #   print(i, end=", ")
 
