e733d19e7685cc707ca95d1f3c52f0f01fd622d1
Unknown <nickbethune@hotmail.com>
2018-02-05 22:59:05 -0800

    add function which parses notes to ordered char array

--- search.py
+++ search.py
@@ -1,15 +1,49 @@
-import xml.etree.ElementTree as ET
+from lxml import etree
 from tkinter.filedialog import askopenfilename
 import tkinter
 
 #tkinter.Tk().withdraw()
 #tree = ET.parse(askopenfilename())
-tree =ET.parse("chopin.xml")
+tree =etree.parse("chopin.xml")
 
 root = tree.getroot()
 
-for child in root:
-    for child2 in child:
-        print(child2.tag)
-        print(child2.attrib)
-    print()
+#print(etree.tostring(root, pretty_print=True))
+
+
+def recursiveElementList(root, i):
+    #for n in range(1, i):
+     # print("    ", end = "")
+
+    root.tag = root.tag.replace("{http://www.music-encoding.org/ns/mei}", "")
+    spaces = ""
+    for n in range(0, i):
+        spaces += "  "
+
+    print(spaces+"<"+root.tag+">")
+    if(root.tag=='note'):
+        print(spaces+" " +root.attrib['pname'])
+
+    spaces = ""
+
+    if len(root):
+        for child in root:
+            recursiveElementList(child, i+1)
+
+
+def getNotesList(root, i, list):
+    #returns list of all notes in order
+
+    #check if element is a note, then if it is, add it's letter to the list.
+    if(root.tag.replace("{http://www.music-encoding.org/ns/mei}", "")=='note'):
+        list.append(root.attrib['pname'])
+        print(root.attrib['pname'])
+    if len(root):
+        for child in root:
+            getNotesList(child, i+1, list)
+    return list
+
+
+for i in getNotesList(root, 0, []):
+    print(i, end=", ")
+
