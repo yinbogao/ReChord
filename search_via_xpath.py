d06c1e0ca7b3a19c42c061b3cac418048e6ec5a9
Unknown <lil@oxy.edu>
2018-02-17 12:28:26 -0800

    amend functions get_notes and notes_on_beam

--- search_via_xpath.py
+++ search_via_xpath.py
@@ -12,34 +12,54 @@ def trial():
     print(r[0].attrib)
     tree.getpath(r[0])
     r[0].getparent()
-
-
-    ls = [child.attrib for child in r[0]]
-    beam_note = []
-    for dic in ls:
-        beam_note += dic['pname']
     return r
 
 
-def notes_on_beam(tree):
-    """return all the notes (in a list) from all the beam in a overall list """
+def get_notes(tree):
+    """return all the notes from the tree """
 
-    # select all the beam element
-    r = tree.xpath('//mei:beam',
+    # select all the note elements
+    r = tree.xpath('//mei:note',
                    namespaces = {'mei': 'http://www.music-encoding.org/ns/mei'})
-    notes_in_beam = []
 
-    # loop through all the element and create a attribute dictionary for each element
-    for i in range(len(r)):
-        ls = [child.attrib for child in r[i]]
+    # loop through r to get attrib dictionary, and select 'pname' key's value add to the list
+    return [note.attrib['pname'] for note in r]
 
-        # add all note from a beam to a list
-        beam_note = [dic['pname'] for dic in ls]
+def notes_on_beam(tree):
+    """return a list of nested list where each nested list is the notes on a beam"""
 
-        # append the beam list to the whole list
-        notes_in_beam += beam_note
-    return notes_in_beam
+    # get a list of all the beam elements
+    r = tree.xpath('//mei:beam',
+                   namespaces={'mei': 'http://www.music-encoding.org/ns/mei'})
+    beam_notes_list = []
 
+    # loop through beam list
+    for beam in r:
 
-print(trial())
+        # get the children of each beam
+        children = beam.getchildren()
+
+        # loop through the children of hte beam
+        # todo: there might be artic ignored here
+        ls = []
+        for child in children:
+
+            # if the child is a note, directly add to the list
+            if child.tag == '{http://www.music-encoding.org/ns/mei}note':
+                ls += child.attrib['pname']
+
+            # else if the child is a rest, add "0" to the list
+            elif child.tag == '{http://www.music-encoding.org/ns/mei}rest':
+                ls += '0'
+
+            # else if the child is a chord, add a list of notes to the list
+            elif child.tag == '{http://www.music-encoding.org/ns/mei}chord':
+                notes = child.getchildren()
+                ls.append([note.attrib['pname'] for note in notes if note.tag == '{http://www.music-encoding.org/ns/mei}note'])
+        beam_notes_list.append(ls)
+
+    return beam_notes_list
+
+# print(trial())
+print(get_notes(tree))
 print(notes_on_beam(tree))
