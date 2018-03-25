daf560ceedc49443464fd63704aedde1cd085775
Lijia Li <lil@oxy.edu>
2018-03-24 22:25:21 -0700

    add direct pass xml to backend w/o writing file

--- ReCord_frontend.py
+++ ReCord_frontend.py
@@ -1,7 +1,7 @@
 from search import prepare_tree, search, find_artic, get_measure
 from lxml import etree
-
 from flask import Flask, request, render_template, flash
+from io import BytesIO
 
 app = Flask(__name__)
 
@@ -16,14 +16,9 @@ def my_form_post():
     if request.form['submit'] == 'Search Snippet':
         snippet = request.form['text']
 
-        # todo: Generate XML, will be replaced
-        xml = snippet.encode()
-        f = open('upload.xml', 'wb')
-        f.write(xml)
-        f.close()
-
+        xml = BytesIO(snippet.encode())
         tree, root = prepare_tree('database/Chopin.xml')
-        inputXML = etree.parse('upload.xml')
+        inputXML = etree.parse(xml)
         input_root = inputXML.getroot()
 
         snippet_measure = search(input_root, tree)
