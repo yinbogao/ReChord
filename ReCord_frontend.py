c8f2c207d9d95a362baa0043f85c740641e4bb75
Unknown <lil@oxy.edu>
2018-04-01 21:13:43 -0700

    modulize file upload

--- ReCord_frontend.py
+++ ReCord_frontend.py
@@ -77,27 +77,8 @@ def my_form_post():
 
     # upload xml file as searching base
     if request.form['submit'] == 'Upload File':
-        print("enter upload_file")
-
-        # check if the post request has the file part
-        if 'base_file' not in request.files:
-            flash('No file part')
-            return redirect(request.url)
-        else:
-            file = request.files['base_file']
-
-            # if user does not select file, browser also submit a empty part without filename
-            if file.filename == '':
-                flash('No selected file')
-                return redirect(request.url)
-
-            # if properly uploaded
-            elif file and allowed_file(file.filename):
-                print("enter if")
-                filename = secure_filename(file.filename)
-                file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-                # todo handle return
-                return redirect(url_for('uploaded_file', filename=filename))
+        filename = upload_file('base_file')
+        return redirect(url_for('uploaded_file', filename=filename))
 
 
 @app.route('/uploads/<filename>')
@@ -105,6 +86,25 @@ def uploaded_file(filename):
     return send_from_directory(app.config['UPLOAD_FOLDER'],
                                filename)
 
+def upload_file(name_tag):
+    # check if the post request has the file part
+    if 'base_file' not in request.files:
+        flash('No file part')
+        return redirect(request.url)
+    else:
+        file = request.files[name_tag]
+
+        # if user does not select file, browser also submit a empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+
+        # if properly uploaded
+        elif file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            # todo handle return
+            return filename
 
 if __name__ == "__main__":
     app.run()
